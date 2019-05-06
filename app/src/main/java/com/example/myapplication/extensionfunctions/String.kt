c3f402fc861444da68738688b57e27f3029cba66
ari <AriSlomovic@gmail.com>
2019-05-06 09:03:40 -0400

    Fixed Email regex checker

--- app/src/main/java/com/example/myapplication/extensionfunctions/String.kt
+++ app/src/main/java/com/example/myapplication/extensionfunctions/String.kt
@@ -7,15 +7,12 @@ import android.text.Html
 import android.text.Spanned
 import android.util.Base64
 import android.util.Log
+import androidx.core.util.PatternsCompat.EMAIL_ADDRESS
 import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun CharSequence.isValidEmail(): Boolean {
-    val emailRegEx =
-        Regex("(?:[a-z0-9!#\$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#\$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
-    return matches(emailRegEx)
-}
+fun CharSequence.isValidEmail() = EMAIL_ADDRESS.matcher(this).matches()
 
 fun String.fromYYYYMMDD(): String {
     if (isEmpty()) return this
